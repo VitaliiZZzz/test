0e104725c853282afaec199da0eb97f166cc069e
VitaliiZZzz <fake.gutov@gmail.com>
2020-02-28 18:23:30 +0200

    add template

--- src/main/java/com/Demo.java
+++ src/main/java/com/Demo.java
@@ -2,12 +2,14 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class Demo {
 
     public static void main(String[] args) {
@@ -17,6 +19,12 @@ public class Demo {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
 }
